08e89723e9f43119f2c2e6dce029d3f01fe63eb4
Antonia Rubio <antoniarubioluque94@outlook.es>
2019-02-11 10:54:10 +0100

    prueba1

---
+++ schoolpy/alumno.py
@@ -0,0 +1,22 @@
+class Alumno():
+    
+    def __init__(self, nombre=None, apellidos=None, edad=None, direccion=None):
+        self.nombre = nombre
+        self.apellidos = apellidos
+        self.edad = edad
+        self.direccion = direccion
+
+    
+
+
+
+
+
+    """def ocuparClase(self):
+        self.ocupada = True
+    
+    def desocuparClase(self):
+        self.ocupada = False
+    
+    def getOcupada(self):
+        return self.ocupada"""
