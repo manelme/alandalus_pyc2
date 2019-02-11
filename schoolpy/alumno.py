8d9a9060b4184c7c0a583976e86678a240c8fd4d
Antonia Rubio <antoniarubioluque94@outlook.es>
2019-02-11 10:48:23 +0100

    Creación de la clase alumno

---
+++ schoolpy/alumno.py
@@ -0,0 +1,16 @@
+class Alumno():
+    
+    def __init__(self, nombre=None, apellidos=None, edad=None):
+        self.nombre = nombre
+        self.apellidos = apellidos
+        self.edad = edad
+
+    
+    """def ocuparClase(self):
+        self.ocupada = True
+    
+    def desocuparClase(self):
+        self.ocupada = False
+    
+    def getOcupada(self):
+        return self.ocupada"""
